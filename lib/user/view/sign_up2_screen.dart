1097affe9f97a5cb3ee80fedb579660035f18ae1
sun <niketjssun31@gmail.com>
2023-01-15 18:29:24 +0900

    feat: 생년월일screen

--- lib/user/view/sign_up2_screen.dart
+++ lib/user/view/sign_up2_screen.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -88,10 +85,14 @@ class _SignUp2ScreenState extends State<SignUp2Screen> {
               SizedBox(height: 40),
               Text('생년월일'),
               const SizedBox(height: 8.0),
-              Row(), //커스텀 드롭다운
+              // CustomDropdown(
+              //   text: 'Call to action',
+              // ),
+              CustomDropdown(text: 'Call to action'),
               SizedBox(
                 height: MediaQuery.of(context).size.width * 0.75,
               ),
+              Text('성별'),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -134,29 +135,202 @@ class _SignUp2ScreenState extends State<SignUp2Screen> {
   }
 }
 
-class BirthDatePicker extends StatelessWidget {
-  final void Function(DateTime) onDateTimeChanged;
-  final String? initDateStr;
-
-  BirthDatePicker({
-    required this.onDateTimeChanged,
-    this.initDateStr,
+class CustomDropdown extends StatefulWidget {
+  final String text;
+  const CustomDropdown({
+    required this.text,
+    super.key,
   });
 
   @override
+  State<CustomDropdown> createState() => _CustomDropdownState();
+}
+
+class _CustomDropdownState extends State<CustomDropdown> {
+  GlobalKey actionKey = GlobalKey();
+  double? height, width, xPosition, yPosition;
+  bool isDropdownOpened = false;
+  late OverlayEntry floatingDropdown;
+
+  @override
+  void initState() {
+    actionKey = LabeledGlobalKey(widget.text);
+    super.initState();
+  }
+
+  void findDropdownData() {
+    RenderBox renderBox =
+        actionKey.currentContext!.findRenderObject() as RenderBox;
+    height = renderBox.size.height;
+    width = renderBox.size.width;
+    Offset offset = renderBox.localToGlobal(Offset.zero);
+    xPosition = offset.dx;
+    yPosition = offset.dy;
+    print('=========================================');
+    print(height);
+    print(width);
+    print(xPosition);
+    print(yPosition);
+    print('=========================================');
+  }
+
+  OverlayEntry _createFloatingDropdown() {
+    return OverlayEntry(builder: (_) {
+      return Positioned(
+        left: xPosition,
+        width: width,
+        top: yPosition! + height!,
+        height: height! * 4, // 수정
+        child: DropDown(
+          itemHeight: height!, // 수정
+        ),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final initDate =
-        DateFormat('yyyy-MM-dd').parse(initDateStr ?? '2000-01-01');
-    return SizedBox(
-      height: 300,
-      child: CupertinoDatePicker(
-        minimumYear: 1900,
-        maximumYear: DateTime.now().year,
-        initialDateTime: initDate,
-        maximumDate: DateTime.now(),
-        onDateTimeChanged: onDateTimeChanged,
-        mode: CupertinoDatePickerMode.date,
+    return GestureDetector(
+      key: actionKey,
+      onTap: () {
+        setState(() {
+          if (isDropdownOpened) {
+            floatingDropdown.remove();
+          } else {
+            findDropdownData();
+            floatingDropdown = _createFloatingDropdown();
+            Overlay.of(context)!.insert(floatingDropdown);
+          }
+
+          isDropdownOpened = !isDropdownOpened;
+        });
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          border: Border.all(color: INPUT_BORDER_COLOR),
+          color: INPUT_BG_COLOR,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Row(
+            children: [
+              Text(
+                widget.text,
+                style: TextStyle(color: Colors.black26),
+              ),
+              Spacer(),
+              Icon(
+                Icons.arrow_drop_down,
+                color: Colors.black38,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+class DropDown extends StatefulWidget {
+  const DropDown({
+    super.key,
+    required this.itemHeight,
+  });
+  final double itemHeight;
+
+  @override
+  State<DropDown> createState() => _DropDownState();
+}
+
+class Date {
+  bool isSelected;
+  final int date;
+  Date({
+    this.isSelected = false,
+    required this.date,
+  });
+}
+
+class _DropDownState extends State<DropDown> {
+  List<Date> years = <Date>[];
+  List months = [];
+  List days = [];
+  @override
+  void initState() {
+    years = getYears();
+    super.initState();
+  }
+
+  List<Date> getYears() {
+    DateTime now = DateTime.now();
+    int year = now.year;
+    int oldYear = year - 100;
+    for (year; year > oldYear; year--) {
+      years.add(Date(date: year, isSelected: false));
+    }
+    return years;
+  }
+
+  List getMonths() {
+    for (int calendar = 1; calendar < 13; calendar++) {
+      months.add(calendar);
+    }
+    return months;
+  }
+
+  List getDays() {
+    for (int calendar = 1; calendar < 32; calendar++) {
+      days.add(calendar);
+    }
+    return days;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: 4 * widget.itemHeight, //수정
+        decoration: BoxDecoration(
+          border: Border.all(color: INPUT_BORDER_COLOR),
+          color: INPUT_BG_COLOR,
+        ),
+        child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+                child: Container(
+                  color:
+                      years[index].isSelected ? Colors.black12 : Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      "${years[index].date}",
+                      style: TextStyle(fontSize: 14, color: Colors.black),
+                    ),
+                  ),
+                ),
+                onTap: () {
+                  setState(() {
+                    for (int i = 0; i < years.length; i++) {
+                      years[i].isSelected = false;
+                    }
+                    years[index].isSelected = true;
+                  });
+                });
+          },
+          itemCount: years.length,
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+// ListView.builder(
+//         itemCount: 100,
+//         itemBuilder: (BuildContext ctx, int idx) {
+//           return Text('Content Number ${idx}');
+//         },
+//       ),
